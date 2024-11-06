009499eeba38a407674237238357870317aec38e
inaciopvhro <inaciopvhro@hotmail.com>
2024-11-06 03:33:17 -0400

    mudar arquivo

--- index.php
+++ index.php
@@ -18,37 +18,36 @@
     <link href="dashboard.css" rel="stylesheet">
   </head>
   <body>
-    <?php
-    $link = mysqli_connect("147.79.86.208","inaciolocal","Inacio@2628","BancoBot");
+    
 
-    if($link === false) {
-      die("ERROR: could not connect. " . mysqli_connect_error());
-    }
-    echo "<br>";
+    <header class='navbar sticky-top bg-dark flex-md-nowrap p-0 shadow' data-bs-theme='dark'>"
+    <a class='navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white' href='#'>Inacio Informatica</a></header>"
 
-    $sql = "SELECT id, contato, nome, grupo, datacadastro FROM contatos";
-    $result = $link->query($sql);
+    <main class='col-md-9 ms-sm-auto col-lg-10 px-md-4'>"
+    <h2>Gerenciamento de Contatos</h2>;
+    <p>Insira algum valor para pesquisa</p>;
+    <input class='form-control' id='myinput' type='text' placeholder='Buscar..'>;
+    <br>;
+    <div class='table-responsive small'>
+    <table class='table table-striped table-sm'>
+    <thead>;
+      <tr>;
+        <th scope='col'>#</th>;
+         <th scope='col'>Contato</th>;
+         <th scope='col'>Nome</th>;
+         <th scope='col'>Grupo</th>;
+         <th scope='col'>Data Cad</th>;
+         </tr>;
+         </thead>;
+         <tbody id='myTable'>;
+         <?php
+          $link = mysqli_connect("147.79.86.208","inaciolocal","Inacio@2628","BancoBot");
 
-    echo "<header class='navbar sticky-top bg-dark flex-md-nowrap p-0 shadow' data-bs-theme='dark'>"
-    echo "<a class='navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white' href='#'>Inacio Informatica</a></header>"
-
-    echo "<main class='col-md-9 ms-sm-auto col-lg-10 px-md-4'>"
-    echo "<h2>Gerenciamento de Contatos</h2>";
-    echo "<p>Insira algum valor para pesquisa</p>";
-    echo "<input class='form-control' id='myinput' type='text' placeholder='Buscar..'>";
-    echo "<br>";
-    echo "<div class='table-responsive small'>"
-    echo "<table class='table table-striped table-sm'>"
-    echo  "<thead>";
-    echo  "<tr>";
-         echo "<th scope='col'>#</th>";
-         echo "<th scope='col'>Contato</th>";
-         echo "<th scope='col'>Nome</th>";
-         echo "<th scope='col'>Grupo</th>";
-         echo "<th scope='col'>Data Cad</th>";
-         echo "</tr>";
-         echo "</thead>";
-         echo "<tbody id='myTable'>";
+          if($link === false) {
+            die("ERROR: could not connect. " . mysqli_connect_error());
+          }
+          $sql = "SELECT id, contato, nome, grupo, datacadastro FROM contatos";
+          $result = $link->query($sql);
             if ($result->num_rows > 0) {
               while($row = $result->fetch_assoc()) {
                 echo "<tr>";
@@ -62,14 +61,16 @@
             } else {
               echo "0 resultados";
             }
-          echo "</tbody>"
-          echo "</table>"
+            mysqli_close($link);
+            ?>
+          </tbody>
+          </table>
 
-          mysqli_close($link);
           
-      echo "</div>"
-    echo "</main>"
-    ?>
+          
+      </div>
+    </main>
+    
   </body>
 <script src="bootstrap.bundle.min.js"></script>
 
